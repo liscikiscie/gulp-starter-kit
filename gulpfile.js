dfa5129d690d7bb05278e4a58a786e147f407bf1
Jakub Wasiljew <websolutionslab@gmail.com>
2017-09-28 01:44:48 +0200

    autoprefixer added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('reload', function () {
     browserSync.reload();
@@ -17,7 +19,13 @@ gulp.task('serve', ['sass'], function () {
 
 gulp.task('sass', function (  ) {
     return gulp.src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            //configuration docs github.com/ai/browserslist  browserl.ist
+            browsers: ['last 3 versions']
+        }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
 });
